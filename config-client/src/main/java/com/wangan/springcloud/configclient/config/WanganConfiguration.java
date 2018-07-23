b11586a4e41168666f7f08f42ec856501745bf52
wangan <il>
2018-07-23 22:54:31 +0800

    change

--- config-client/src/main/java/com/wangan/springcloud/configclient/config/WanganConfiguration.java
+++ config-client/src/main/java/com/wangan/springcloud/configclient/config/WanganConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @author wangan on 2018/7/12
  * @description
  */
+@Configuration
 @EnableConfigurationProperties(User.class)
 public class WanganConfiguration {
-
-	private final User user;
-
-	@Autowired
-	public WanganConfiguration(User user) {
-		this.user = user;
-	}
 
 }
