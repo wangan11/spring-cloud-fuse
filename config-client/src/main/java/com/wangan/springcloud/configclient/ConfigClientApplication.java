c49510cdbe8fc0a347cf1a6e99b19eef9f6824fc
wa <15155313869@163.com>
2018-07-24 14:17:16 +0800

    aaaaaaaaaaaa

--- config-client/src/main/java/com/wangan/springcloud/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/wangan/springcloud/configclient/ConfigClientApplication.java
@@ -2,12 +2,14 @@ package com.wangan.springcloud.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wangan on 2018/7/23
  * @description
  */
  @SpringBootApplication
+ @EnableEurekaClient
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
